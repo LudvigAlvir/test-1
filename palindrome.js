8abe5cbe89ea32fe14a08a0500cb637f7ad6dd5f
hyseh <edgyhyseh@gmail.com>
2024-04-17 11:57:06 +0200

    palindrome success

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,21 @@
 //takes in a word and returns true if the word is a palindrome, false if it is not
-export function isWordPalindrome(str) {}
+export function isWordPalindrome(str) {
+  const word = str.toLowerCase();
+  const reverse = word.split('').reverse().join('');
+
+  if (word === reverse) {
+    return true;
+  }
+  return false;
+}
+
 //takes in a sentence and returns true if the sentence is a palindrome, false if it is not
-export function isSentencePalindrome(str) {}
+export function isSentencePalindrome(str) {
+  const sentence = str.toLowerCase().split(' ').join('');
+  const reverse = sentence.split('').reverse().join('');
+
+  if (sentence === reverse) {
+    return true;
+  }
+  return false;
+}
